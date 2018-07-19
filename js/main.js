f466cfa08bbc28f1428cfe2d4cf0c2b05e691bba
kaimipono <weforgottenwhoweare@gmail.com>
2018-07-19 19:25:06 +0300

    Приложение работает для статических размеров поля

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ class Game {
         this.height = height;
         this.activeCells = [];
         this.openedCells = 0;
+        this.app = document.getElementById('app');    // ссылка на игровое поле
     }
 
     init() {
         this.showGameField();
-        // this.playGame();
+        this.playGame();
     }
 
     // Отобразить игровое поле
@@ -30,7 +31,7 @@ class Game {
         }
 
         // Формируем игровое поле для текущих размеров
-        const app = document.getElementById('app');
+        // const app = document.getElementById('app');
 
         for (let i = 0; i < this.height; i++) {
             let row = document.createElement('tr');
@@ -45,15 +46,90 @@ class Game {
                 cell.appendChild(img);
                 row.appendChild(cell);
             }
-            app.appendChild(row);
+            this.app.appendChild(row);
         }
     }
 
+    playGame() {
+
+        // Кликаем по полю
+        this.app.addEventListener('click', (event) => {
+            // сохраняем нажатую ячейку
+            let cell = event.target.parentNode;
+            // если ячейка не "заиграна", то делаем для нее проверку
+            if (cell.className !== 'cell complete') {
+                this.checkSelectedCell(cell);
+            }
+        });
+        
+    }
     
+    checkSelectedCell(cell) {
+        switch (this.activeCells.length) {
+            case 0:
+                this.addActiveCell(cell);
+                break;
+            
+            case 1:
+                if (this.activeCells[0] === cell) {
+                    return;
+                } else if (this.isEqualCells(this.activeCells[0], cell)) {
+                    this.activeCells[0].classList.remove('active');
+                    this.activeCells[0].classList.add('complete');
+                    cell.classList.add('complete');
+                    this.activeCells = [];
+                    this.openedCells += 2;
+                    if (this.openedCells === this.width * this.height){
+                        this.finishGame();
+                    }
+                } else {
+                    this.addActiveCell(cell);
+                }
+                break;
+            
+            case 2:
+                this.hideActiveCells(this.activeCells);
+                this.addActiveCell(cell);
+                break;
+
+        }
+    }
+
+    addActiveCell(cell) {
+        cell.classList.add('active');
+        this.activeCells.push(cell);
+    }
+
+    isEqualCells(firstCell, secondCell) {
+        return firstCell.lastChild.src === secondCell.lastChild.src;
+    }
+
+    hideActiveCells (arrayActiveCells) {
+        arrayActiveCells.map(item => {
+            item.classList.remove('active');
+        });
+        this.activeCells = [];
+    }
+
+    finishGame() {
+        const modal = document.getElementById('modal');
+        const btnClose = document.getElementsByClassName('close')[0];
+
+        modal.style.display = 'block';
+
+        btnClose.onclick = () => modal.style.display = 'none';
+
+        window.onclick = (event) => {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        };
+
+    }
 
 }
 
-const game = new Game(8, 3);
+const game = new Game(2, 3);
 
 game.init();
 
